4e7daa3a915318366550dc152014d1754bc72fbb
yanxunlong <167728341+yanxunlong@users.noreply.github.com>
2025-08-06 18:45:59 +0800

    fixed test code

--- test/CCN.test.js
+++ test/CCN.test.js
@@ -1,5 +1,6 @@
 const { expect } = require("chai");
 const { ethers } = require("hardhat");
+const { parseEther } = require("ethers");
 
 describe("CCNCarnival", function () {
   let carnival, owner, user;
@@ -17,22 +18,22 @@ describe("CCNCarnival", function () {
 
   it("Should accept payments", async function () {
     await carnival.registerStall("Food Stall", 3);
-    await carnival.connect(user).payToStall(1, { value: ethers.utils.parseEther("1") });
+    await carnival.connect(user).payToStall(1, { value: parseEther("1") });
     const paid = await carnival.payments(1, user.address);
-    expect(paid).to.equal(ethers.utils.parseEther("1"));
+    expect(paid).to.equal(parseEther("1"));
   });
 
   it("Should allow refunds by owner", async function () {
     await carnival.registerStall("Food Stall", 3);
-    await carnival.connect(user).payToStall(1, { value: ethers.utils.parseEther("1") });
-    await carnival.refundUser(1, user.address, ethers.utils.parseEther("0.5"));
+    await carnival.connect(user).payToStall(1, { value: parseEther("1") });
+    await carnival.refundUser(1, user.address, parseEther("0.5"));
     const paid = await carnival.payments(1, user.address);
-    expect(paid).to.equal(ethers.utils.parseEther("0.5"));
+    expect(paid).to.equal(parseEther("0.5"));
   });
 
   it("Should allow withdrawals after event ends", async function () {
     await carnival.registerStall("Food Stall", 1);
-    await carnival.connect(user).payToStall(1, { value: ethers.utils.parseEther("1") });
+    await carnival.connect(user).payToStall(1, { value: parseEther("1") });
     await carnival.withdraw(1, 3);
     const stall = await carnival.stalls(1);
     expect(stall.withdrawn).to.be.true;
