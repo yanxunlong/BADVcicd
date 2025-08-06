bf72cb1b40fa330e6fc3e52f513831a261e66326
yanxunlong <167728341+yanxunlong@users.noreply.github.com>
2025-08-06 18:33:02 +0800

    new fix to deploy.js

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,12 +1,15 @@
 require("@nomicfoundation/hardhat-toolbox");
 require("dotenv").config();
 
+const SEPOLIA_URL = process.env.SEPOLIA_URL || "";
+const PRIVATE_KEY = process.env.PRIVATE_KEY || "";
+
 module.exports = {
   solidity: "0.8.23",
   networks: {
     sepolia: {
-      url: process.env.SEPOLIA_URL,
-      accounts: [process.env.PRIVATE_KEY]
+      url: SEPOLIA_URL,
+      accounts: PRIVATE_KEY ? [PRIVATE_KEY] : []
     }
   }
 };
